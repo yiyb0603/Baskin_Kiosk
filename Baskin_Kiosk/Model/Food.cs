fed2382b21a0e7e98077d1baee48d6adf92aaba7
yiyb0603 <yiyb0603@gmail.com>
2020-10-14 14:15:16 +0900

    [ADD] FoodCount, select, total amount
    음식 수량 증감소, 선택메뉴 삭제, 총금액을 추가하였습니다

---
+++ Baskin_Kiosk/Model/Food.cs
@@ -0,0 +1,31 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baskin_Kiosk.Common
+{
+    public class Food : BindableBase
+    {
+        public Category category { get; set; }
+        public String imageSrc { get; set; }
+        public String foodName { get; set; }
+        public int page { get; set; }
+
+        private int _price;
+        public int price 
+        {
+            get => _price;
+            set => SetProperty(ref _price, value);
+        }
+
+        private int _count = 1;
+        public int count 
+        {
+            get => _count;
+            set => SetProperty(ref _count, value);
+        }
+    }
+}
