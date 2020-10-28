85af5a1f5a5d0a6bc70c60a55c0b40ad087f68a0
yiyb0603 <yiyb0603@gmail.com>
2020-10-28 12:26:48 +0900

    [FIX] Order price
    DB에 총가격으로 들어가던 정보를 각각 메뉴 가격으로 변경했습니다

--- Baskin_Kiosk/Model/OrderModel.cs
+++ Baskin_Kiosk/Model/OrderModel.cs
@@ -15,6 +15,6 @@ namespace Baskin_Kiosk.Model
         public int userId { get; set; }
         public DateTime orderTime { get; set; }
         public int menuId { get; set; }
-        public int totalPrice { get; set; }
+        public int price { get; set; }
     }
 }
