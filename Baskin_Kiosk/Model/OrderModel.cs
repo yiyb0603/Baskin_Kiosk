048447d4f3f601f881158244bebcaa1375abaade
kirrard <35512725+kirrard@users.noreply.github.com>
2020-11-03 10:48:13 +0900

    [ADD] 주문 화면 주문번호 표기
    DB에서 주문번호 값을 받아와 표기합니다.

--- Baskin_Kiosk/Model/OrderModel.cs
+++ Baskin_Kiosk/Model/OrderModel.cs
@@ -16,5 +16,6 @@ namespace Baskin_Kiosk.Model
         public DateTime orderTime { get; set; }
         public int menuId { get; set; }
         public int price { get; set; }
+        public int orderNum { get; set; }
     }
 }
