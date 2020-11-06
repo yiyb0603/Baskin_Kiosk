80632ef7b7bcee8330b98ae58d3365325ebecbdf
kirrard <35512725+kirrard@users.noreply.github.com>
2020-11-06 11:30:10 +0900

    [ADD] 자리 선택 페이지 구조 변경
    Button 배열을 사용한 자리 선택->Listbox를 사용한 자리선택으로 수정

--- Baskin_Kiosk/ViewModel/SeatViewModel.cs
+++ Baskin_Kiosk/ViewModel/SeatViewModel.cs
@@ -3,11 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Threading;
 
 namespace Baskin_Kiosk.ViewModel
 {
     class SeatViewModel
     {
+        DispatcherTimer useTimer = new DispatcherTimer();
 
+        public bool used = false;
+        public int seatNumber { get; set; }
+        public int _time = 0;
+        public int time
+        {
+            get
+            {
+                return _time;
+            }
+            set
+            {
+                _time = value;
+            }
+        }
+
+        public SeatViewModel()
+        {
+            useTimer.Interval = TimeSpan.FromSeconds(1);
+            useTimer.Tick += UseTimer_Tick;
+        }
+
+        public void UseSeat()
+        {
+            used = true;
+            useTimer.Start();
+        }
+
+        private void UseTimer_Tick(object sender, EventArgs e)
+        {
+            time--;
+            if (time <= 0)
+            {
+                useTimer.Stop();
+                used = false;
+            }
+        }
     }
 }
